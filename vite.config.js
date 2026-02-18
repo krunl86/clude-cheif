01efd0887cb71dafad9864be8883af4f728177ee
krunl86 <krunal.kachhua@gmail.com>
2026-02-18 19:48:55 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig( {
+  plugins: [ react() ],
+  base: '/clude- cheif/',   // 👈 REQUIRED for GitHub Pages
+
+} )
