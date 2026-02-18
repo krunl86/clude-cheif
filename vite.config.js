e2f25acd52cb53f3fe65cfc02fd61e0e21a26c6d
krunl86 <krunal.kachhua@gmail.com>
2026-02-18 20:10:27 +0530

    vite.config.js udated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig( {
   plugins: [ react() ],
-  base: '/clude- cheif/',   // 👈 REQUIRED for GitHub Pages
+  base: '/clude-gcheif/',   // 👈 REQUIRED for GitHub Pages
 
 } )
